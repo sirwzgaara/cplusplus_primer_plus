08b5f75cb8f844e163279bfb6203f61f8fee0d1a
e450c-linux <1819644289@qq.com>
2018-10-19 13:30:28 +0800

    modified:   chapter11/a.out 	modified:   chapter11/helloworld.cpp 	modified:   chapter12/a.out 	modified:   chapter12/helloworld.cpp

--- chapter11/helloworld.cpp
+++ chapter11/helloworld.cpp
@@ -2,9 +2,35 @@
 #include <iostream>
 using namespace std;
 
+class A
+{
+private:
+    int i;
+public:
+    A(int _i = 0) : i(_i){}
+    A operator+(A & a);
+    operator double() const;
+};
+
+A A::operator+(A & a)
+{
+    cout << i << endl;
+    A tmp;
+    tmp.i = i + a.i;
+    return tmp;
+}
+
+A::operator double() const
+{
+    return 10.1;
+}
+
 void func()
 {
-
+    A a(10);
+    double g = a;
+    cout << g << endl;
+    cout << a << endl;
 }
 
 int main()
--- chapter12/helloworld.cpp
+++ chapter12/helloworld.cpp
@@ -1,21 +1,52 @@
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Test
 {
+private:
+    char * s;
 public:
-    virtual void lala()=0;
-} ;
-
-class Child : public Test
-{
-public:
-    virtual void lala(){}
+    Test(char * _s);
+    Test(const Test & t);
+    ~Test();
+    Test & operator=(const Test & t);
 };
+
+Test::Test(char * _s)
+{
+    int len = strlen(_s);
+    s = new char [len + 1];
+    strcpy(s, _s);
+}
+
+Test::Test(const Test & t)
+{
+    int len = strlen(t.s);
+    s = new char [len + 1];
+    strcpy(s, t.s);
+}
+
+Test & Test::operator=(const Test & t)
+{
+    if (this != &t)
+    {
+        Test local(t);
+        char * tmp = local.s;
+        local.s = s;
+        s = tmp;
+    }
+    return *this;
+}
+
+Test::~Test()
+{
+    delete [] s;
+}
 
 int main()
 {
-    Child tmp;
+    cout << sizeof(Test) << endl;
     return 1;
 }
