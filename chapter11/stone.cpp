f925e515b0cdb9bd7868c42c89211ef5bbd2861a
e450c-linux <1819644289@qq.com>
2018-11-20 20:14:43 +0800

    modified:   chapter11/stone.cpp

--- chapter11/stone.cpp
+++ chapter11/stone.cpp
@@ -10,20 +10,22 @@ private:
     double pds_left;
     double pounds;
 public:
-    explicit Stonewt(double lbs);
+    explicit Stonewt(double lbs);    //通过加explicit防止隐式类型转换
     Stonewt(int stn, double lbs);
     Stonewt();
-    ~Stonewt();
+    ~Stonewt(){}
     void show_lbs() const;
     void show_stn() const;
-    explicit operator double() const;
+    /* 转换函数 */
+    operator double() const;
     operator int() const;
 };
 
+/* 此函数存在让Stonewt mycat; mycat = 19.6这种赋值成为可能 */
 Stonewt::Stonewt(double lbs)
 {
-    stone = int (lbs) / Lbs_per_stn;
-    pds_left = int (lbs) / Lbs_per_stn + lbs - int (lbs);
+    stone = int(lbs) / Lbs_per_stn;
+    pds_left = int (lbs) % Lbs_per_stn + lbs - int(lbs);
     pounds = lbs;
 }
 
@@ -36,22 +38,23 @@ Stonewt::Stonewt(int stn, double lbs)
 
 Stonewt::Stonewt()
 {
-    stone = pounds = pds_left = 0;
-}
-
-Stonewt::~Stonewt()
-{
-
+    stone = pds_left = pounds = 0;
 }
 
 void Stonewt::show_stn() const
 {
-    cout << stone << " stone, " << pds_left << " pounds" << endl;
+    cout << stone << " stone, " << pds_left << " pounds\n";
 }
 
 void Stonewt::show_lbs() const
 {
-    cout << pounds << " pounds" << endl;
+    cout << pounds << " pounds\n";
+}
+
+Stonewt::operator int() const
+{
+    /* 一个四舍五入的方法 */
+    return int(pounds + 0.5);
 }
 
 Stonewt::operator double() const
@@ -59,20 +62,9 @@ Stonewt::operator double() const
     return pounds;
 }
 
-Stonewt::operator int() const
-{
-    return int (pounds + 0.5);
-}
-
-void func()
-{
-    Stonewt mycat;
-    mycat = (Stonewt)19.6;
-    int n = mycat;
-}
-
 int main()
 {
-    func();
+    Stonewt t(19.6);
+    double n = t;
     return 1;
 }
